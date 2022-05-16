852f4ae439c1c2e08f47f2c3d380d71f1420243d
YouKnowKim <h3969@naver.com>
2022-05-16 17:56:08 +0900

    d

---
+++ algorithm/day01/Main.java
@@ -0,0 +1,23 @@
+package day01;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		Solution solution = new Solution();
+		System.out.println(solution.solution(input));
+	}
+}
+
+
+class Solution {
+	int solution(int input) {
+		int answer=0;
+		// TO DO:
+		
+		return answer;
+	}
+}
