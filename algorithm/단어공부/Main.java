9414e91b751dfb8c9848949fce5d1c9de2e6c37e
YouKnowKim <h3969@naver.com>
2022-05-19 18:53:21 +0900

    e

--- algorithm/단어공부/Main.java
+++ algorithm/단어공부/Main.java
@@ -7,7 +7,46 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		String str = scanner.nextLine();
+		String str = scanner.nextLine().toLowerCase();
+		char[] alphabets = new char[30];
+		int[] checkList = new int[30];
+		char ab = 'a';
+		int i=0;
+		while(ab <= 'z') {
+			alphabets[i] = ab;
+			i++;
+			ab = (char)(ab+1);
+		}
 		
+		for(int j=0; j<str.length(); j++) {
+			for(char x : alphabets) {
+				
+			}
+		}
+		
+		
+//		Map<String,Integer> map = new HashMap<String,Integer>();
+//		Set<String> set = new HashSet<String>();
+//		
+//		for(int i=0; i<str.length(); i++) {
+//			set.add(str.charAt(i)+"");
+//		}
+//		
+//		for(String s : set) {
+//			map.put(s, 0);
+//		}
+//		
+//		for(int i=0; i<str.length(); i++) {
+//			if(map.containsKey(str.charAt(i)+"")) {
+//				map.put(str.charAt(i)+"", map.get(str.charAt(i)+"")+1);
+//			}
+//		}
+//		for(String s : map.keySet()) {
+//			System.out.println(s + " : " + map.get(s));
+//		}
 	}
 }
+
+
+
+
